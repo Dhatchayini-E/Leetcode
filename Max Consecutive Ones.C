80f97e8137b9ec10821244eeeedc95c613198ec6
Dhatchayini-E <107349829+Dhatchayini-E@users.noreply.github.com>
2022-12-30 14:09:52 +0530

    Create Max Consecutive Ones.C

---
+++ Max Consecutive Ones.C
@@ -0,0 +1,20 @@
+int findMaxConsecutiveOnes(int* nums, int numsSize){
+    int count=0,maxCount=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]==1)
+        {
+            count++;
+        }
+        else
+        {
+            count=0;
+        }
+        if(count>maxCount)
+        {
+            maxCount=count;
+        }
+    }
+    return maxCount;
+
+}
